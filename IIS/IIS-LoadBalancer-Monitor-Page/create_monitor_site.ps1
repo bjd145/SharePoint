614fecafe330e8489c8748299136bd4e5e33eb12
Brian Denicola <bjd145@yahoo.com>
2019-10-31 17:28:38 -0500

    initial commit

---
+++ IIS/IIS-LoadBalancer-Monitor-Page/create_monitor_site.ps1
@@ -0,0 +1,6 @@
+﻿$dst = "D:\Web\Monitoring"
+$src = Join-Path $PWD.Path "Site"
+
+New-Item -Path $dst -ItemType Directory
+Copy-Item "$src\*.*" -Destination $dst -Verbose -Recurse
+..\WebFarm\Create-IIS-WebApplication.ps1 -config .\monitor.xml 
